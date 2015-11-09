7aa7ba47c35b30fab7b01a13b8902b513b6ab357
Shad_Hasan <Shad_Hasan@BLRKEC259343D.ad.infosys.com>
2015-11-09 16:16:50 +0530

    Partially coded added for PrepareDisk in Make disk. Next step is to update encode significate and spaceleft byte update.

--- VirtualDisk/src/diskvirtual/AddFile.java
+++ VirtualDisk/src/diskvirtual/AddFile.java
@@ -32,7 +32,7 @@ public class AddFile {
 		
 	}
 	public AddFile(){
-		
+		fileWriteTest();
 	}
 	public void ReadDisk()
 	{
@@ -40,18 +40,74 @@ public class AddFile {
 		try{
 			aFile     = new RandomAccessFile("D:\\VD\\vd1.shd", "rw");
 			FileChannel      inChannel = aFile.getChannel();
-			ByteBuffer buf = ByteBuffer.allocate(1024*1024);
-
-			int bytesRead;
+			ByteBuffer buf = ByteBuffer.allocate(5);
+			//aFile.seek(1024*1024*1024-1);
+			aFile.seek(0);
+			System.out.print(aFile.read());
+			//Comment code return number of byte read through bytebuffer allocated
+			/*int bytesRead;
 			do{
 				bytesRead= inChannel.read(buf);
 			}while (bytesRead != -1 && buf.hasRemaining());
-			System.out.print(bytesRead);
+			System.out.print(bytesRead);*/
 			aFile.close();
 			}
 			catch(IOException e)
 			{
 				e.printStackTrace();
 			}
+	}
+	private void fileWriteTest()
+	{
+		Byte [] b= new Byte[5];
+		b[0] = 0x01;
+		b[1] = 0x23;
+		b[2] = 0x66;
+		b[3] = 0x54;
+		b[4] = 0x11;
+		RandomAccessFile aFile;
+		try{
+			aFile     = new RandomAccessFile("D:\\VD\\vd1.shd", "rw");
+			String data= "Hello I am only person exist in world";
+			long fileLength = aFile.length();
+			byte b1 = (byte)255;
+			System.out.print(orginalByte(b1)+" ");
+			aFile.seek(fileLength-1);
+			int IndexBit = aFile.read();	//Index bit
+			aFile.seek(fileLength-2);
+			int Significate = aFile.read();
+			aFile.seek(fileLength-3);
+			int MSpaceLeft = aFile.read();
+			aFile.seek(fileLength-4);
+			int KSpaceLeft = aFile.read();
+			aFile.seek(fileLength-5);
+			int MNextOffset = aFile.read();
+			aFile.seek(fileLength-6);
+			int KNextOffset = aFile.read();
+			
+			
+			System.out.print(IndexBit+""+MNextOffset+""+KNextOffset+""+MSpaceLeft+""+KSpaceLeft);
+			
+			aFile.close();
+		}
+		catch(IOException e)
+		{
+			e.printStackTrace();
+		}
+	}
+	private int orginalByte(byte b1)
+	{
+		if(b1<0)
+		return (256- (~(b1-1)));
+		else
+			return b1;
+	}
+	private int IntegerSplitFour(byte b1)
+	{
+		int NextoffsetSignificantM = b1 & 0x30>>4;
+		int NextoffsetSignificantK = b1 & 0xC0>>6;
+		int SpaceLeftSignificantM = b1 & 0x03;
+		int SpaceLeftSignificantK = b1 & 0x0C>>2;
+		return NextoffsetSignificantM;
 	}
 }
--- VirtualDisk/src/diskvirtual/MainReadWrite.java
+++ VirtualDisk/src/diskvirtual/MainReadWrite.java
@@ -6,14 +6,14 @@ public class MainReadWrite {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		/*try {
-			new MakeDisk("vd1.shd", 1, "GB");
+		try {
+			new MakeDisk("vd1.shd", 1024, "KB");
 			
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
-		}*/
-		new AddFile().ReadDisk();
+		}
+		//new AddFile();
 	}
 
 }
